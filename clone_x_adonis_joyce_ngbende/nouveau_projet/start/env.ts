c6692d6b804e3339702d131108acc413dec3a954
joyce <ngbendej@gmail.com>
2025-11-02 05:55:59 +0100

    revu du fichier pour rendre le smtp optionnel est regler le probleme sur render

--- clone_x_adonis_joyce_ngbende/nouveau_projet/start/env.ts
+++ clone_x_adonis_joyce_ngbende/nouveau_projet/start/env.ts
@@ -41,6 +41,9 @@ export default await Env.create(new URL('../', import.meta.url), {
   | Variables for configuring the mail package
   |----------------------------------------------------------
   */
-  SMTP_HOST: Env.schema.string(),
-  SMTP_PORT: Env.schema.string()
+  SMTP_HOST: Env.schema.string.optional(),
+  SMTP_PORT: Env.schema.string.optional(),
+  MAIL_FROM_ADDRESS: Env.schema.string.optional(),
+  MAIL_FROM_NAME: Env.schema.string.optional(),
+  SENDGRID_API_KEY: Env.schema.string.optional(),
 })
